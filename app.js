5a5f853256fbed14d146d0a8b5392d8e3268423c
unknown <gustavo_raniele@hotmail.com>
2023-02-23 18:00:55 -0300

    First Commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const env = require('dotenv')
+env.config()
+
+app.set('view engine', 'ejs')
+app.set('views', './public/view')
+app.use('/public/script', express.static('./public/script'))
+
+app.get('/', ( req, res ) =>{
+    res.render('index')
+})
+
+app.listen(process.env.PORT, (err) => {
+    if(err) console.log('Erro ao abrir o serve')
+    console.log('Serve Aberto')
+})
---
+++ public/script/map.js
@@ -0,0 +1,18 @@
+if ("geolocation" in navigator) {
+    navigator.geolocation.watchPosition(teste);
+} else {
+    alert("Por favor aceite o token de geolocalização");
+}
+
+function teste(position){
+    var map = L.map('map').setView([position.coords.latitude, position.coords.longitude], 13);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 100,
+    }).addTo(map);
+
+    L.marker([position.coords.latitude, position.coords.longitude], 13).addTo(map)
+        .bindPopup('Teste de Pop')
+        .openPopup();
+}
+
